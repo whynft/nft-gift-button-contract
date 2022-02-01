1d5a4648e4e973582685d42d8170f59504c4c1a7
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2022-02-01 23:25:14 +0300

    add tests to erc721Fake and finally, test performTransferNFT

--- test/Darilka.js
+++ test/Darilka.js
@@ -14,7 +14,7 @@ const defaultAmountForBook = "10000000000000000"
 
 
 contract("Darilka", (accounts) => {
-    let [alice, bob, backend] = accounts;
+    let [alice, bob, backend, someOneWhoDeployedNftContract] = accounts;
     let contractInstance;
 
     beforeEach(async () => {
@@ -35,8 +35,9 @@ contract("Darilka", (accounts) => {
         })
 
         it("Fake erc721 should be able to safeMint", async () => {
+            const tokenId = 1;
             const erc721FakeContractInstance = await Erc721Fake.new({from: backend});
-            const result = await erc721FakeContractInstance.safeMint(alice, 1, {from: alice});
+            const result = await erc721FakeContractInstance.safeMint(alice, tokenId, {from: alice});
             assert.equal(result.receipt.status, true);
         })
     })
@@ -87,5 +88,115 @@ contract("Darilka", (accounts) => {
         })
     })
 
-    // todo: test performTransferNFT
+    context("Test performTransfer with true erc721 (erc721 without miscellanies) [complicated tests]", async () => {
+        it("Should transfer with success", async () => {
+            // Alice mint
+            const tokenId = 1;
+            const erc721FakeContractInstance = await Erc721Fake.new({from: someOneWhoDeployedNftContract});
+            await erc721FakeContractInstance.safeMint(alice, 1, {from: alice});
+            // Alice approve nft for Darilka contract
+            const approved = await erc721FakeContractInstance.approve(contractInstance.address, tokenId, {from: alice});
+            assert.equal(approved.receipt.status, true);
+            // Alice set password
+            const hashPassword = web3.utils.keccak256(defaultPassword);
+            const resultOfConfirmation = await contractInstance.setConfirmation(
+                erc721FakeContractInstance.address,
+                tokenId,
+                hashPassword,
+                {from: alice, value: defaultSenderCost}
+            )
+            assert.equal(resultOfConfirmation.receipt.status, true);
+            // Bob as gift receiver request backend (backend checks verifications code) and book the gift for bob address
+            const result = await contractInstance.bookTransfer(
+                bob,
+                erc721FakeContractInstance.address,
+                tokenId,
+                {from: backend, value: defaultAmountForBook},
+            )
+            assert.equal(result.receipt.status, true);
+            // Bob initiate safeTransferFrom of erc721 by performTransferNFT in Darilka contract
+            // to note: he pass plain password
+            const transferred = await contractInstance.performTransferNFT(
+                alice,
+                bob,
+                erc721FakeContractInstance.address,
+                tokenId,
+                defaultPassword,
+                {from: bob},
+            )
+            assert.equal(transferred.receipt.status, true)
+            const newOwner = await erc721FakeContractInstance.ownerOf(tokenId);
+            assert.equal(newOwner, bob);
+        })
+
+        it("Should not transfer with wrong password [confirmation]", async () => {
+            // Alice mint
+            const tokenId = 1;
+            const erc721FakeContractInstance = await Erc721Fake.new({from: someOneWhoDeployedNftContract});
+            await erc721FakeContractInstance.safeMint(alice, 1, {from: alice});
+            // Alice approve nft for Darilka contract
+            const approved = await erc721FakeContractInstance.approve(contractInstance.address, tokenId, {from: alice});
+            assert.equal(approved.receipt.status, true);
+            // Alice set password
+            const hashPassword = web3.utils.keccak256(defaultPassword);
+            const resultOfConfirmation = await contractInstance.setConfirmation(
+                erc721FakeContractInstance.address,
+                tokenId,
+                hashPassword,
+                {from: alice, value: defaultSenderCost}
+            )
+            assert.equal(resultOfConfirmation.receipt.status, true);
+            // Bob as gift receiver request backend (backend checks verifications code) and book the gift for bob address
+            const result = await contractInstance.bookTransfer(
+                bob,
+                erc721FakeContractInstance.address,
+                tokenId,
+                {from: backend, value: defaultAmountForBook},
+            )
+            assert.equal(result.receipt.status, true);
+            // Bob initiate safeTransferFrom of erc721 by performTransferNFT in Darilka contract
+            // to note: he pass plain password
+            await utils.shouldThrow(contractInstance.performTransferNFT(
+                alice,
+                bob,
+                erc721FakeContractInstance.address,
+                tokenId,
+                "WrongPassword",
+                {from: bob},
+            ))
+        })
+
+        it("Should not transfer without missing setConfirmation step [password on nft] but with book", async () => {
+            // Alice mint
+            const tokenId = 1;
+            const erc721FakeContractInstance = await Erc721Fake.new({from: someOneWhoDeployedNftContract});
+            await erc721FakeContractInstance.safeMint(alice, 1, {from: alice});
+            // Alice approve nft for Darilka contract
+            const approved = await erc721FakeContractInstance.approve(contractInstance.address, tokenId, {from: alice});
+            assert.equal(approved.receipt.status, true);
+            // Alice does not set password...
+            // Bob as gift receiver request backend (backend checks verifications code) and book the gift for bob address
+            const result = await contractInstance.bookTransfer(
+                bob,
+                erc721FakeContractInstance.address,
+                tokenId,
+                {from: backend, value: defaultAmountForBook},
+            )
+            assert.equal(result.receipt.status, true);
+            // Bob initiate safeTransferFrom of erc721 by performTransferNFT in Darilka contract
+            // to note: he pass plain password
+            await utils.shouldThrow(contractInstance.performTransferNFT(
+                alice,
+                bob,
+                erc721FakeContractInstance.address,
+                tokenId,
+                defaultPassword,
+                {from: bob},
+            ))
+        })
+    })
+
+    context("Test performTransfer with miscellanies erc721 [complicated tests]", async () => {
+
+    })
 })
